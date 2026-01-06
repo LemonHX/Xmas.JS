c441c6f0446c314030926da6bfbae137c298d84c
lemonhx <lemonhx@lemonhx.moe>
2026-01-06 13:28:28 +0800

    fix: sclone

--- modules/src/serdeserclone.rs
+++ modules/src/serdeserclone.rs
@@ -1,15 +1,12 @@
-use rsquickjs::{prelude::Func, ArrayBuffer, Ctx, Result, Value};
+use rsquickjs::{prelude::Func, ArrayBuffer, Ctx, Result, TypedArray, Value};
 
 pub fn init<'js>(ctx: &Ctx<'js>) -> Result<()> {
     let globals = ctx.globals();
     globals.set(
         "internalSerialize",
         Func::from(
-            |ctx: Ctx<'js>, value: rsquickjs::Value<'js>| -> Result<rsquickjs::ArrayBuffer<'js>> {
-                let array_buffer = value.serialize()?;
-                let array_buffer: rsquickjs::ArrayBuffer<'js> =
-                    rsquickjs::ArrayBuffer::new(ctx.clone(), array_buffer)?;
-                Ok(array_buffer)
+            |ctx: Ctx<'js>, value: rsquickjs::Value<'js>| -> Result<rsquickjs::Value<'js>> {
+                value.serialize(ctx)
             },
         ),
     )?;
@@ -18,25 +15,7 @@ pub fn init<'js>(ctx: &Ctx<'js>) -> Result<()> {
         "internalDeserialize",
         Func::from(
             |ctx: Ctx<'js>, value: rsquickjs::Value<'js>| -> Result<rsquickjs::Value<'js>> {
-                if value.is_array_buffer() {
-                    let vec = ArrayBuffer::from_value(value).unwrap();
-                    let vec = match vec.as_bytes() {
-                        Some(b) => b,
-                        None => {
-                            return Err(rsquickjs::Error::new_from_js(
-                                "TypeError",
-                                "Failed to get bytes from ArrayBuffer",
-                            ))
-                        }
-                    };
-                    let value = Value::deserialize(ctx, vec)?;
-                    return Ok(value);
-                } else {
-                    return Err(rsquickjs::Error::new_from_js(
-                        "TypeError",
-                        "Value must be an ArrayBuffer",
-                    ));
-                }
+                value.deserialize(ctx)
             },
         ),
     )?;
@@ -45,9 +24,7 @@ pub fn init<'js>(ctx: &Ctx<'js>) -> Result<()> {
         "structuredClone",
         Func::from(
             |ctx: Ctx<'js>, value: rsquickjs::Value<'js>| -> Result<rsquickjs::Value<'js>> {
-                let vec = value.serialize()?;
-                let value = Value::deserialize(ctx, &vec)?;
-                Ok(value)
+                value.structured_clone(ctx)
             },
         ),
     )?;
